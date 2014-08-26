0f295217c8a37897d1280c850ac4671bcacc76b0
Pau Maraví <paumb85@gmail.com>
2014-08-26 19:24:37 +0200

    Create upFileMR.sh

---
+++ upFileMR.sh
@@ -0,0 +1,57 @@
+#!/bin/bash                                                                                                     
+
+###################################################################################                             
+#Autor: PauMB                                                                     #                             
+#Programa:Replica del comando "SCP", para subir 'un' archivo a una maquina remota #                             
+#         No re se realiza con 'expect' por seguridad                             #                             
+###################################################################################                             
+
+
+#funcion que nos indica como utilizar el script                                                                 
+function help {
+    echo "Uso: ./upFileMR Nombre_Maquina Archivo"
+    exit 1
+}
+
+
+argumentos=$#
+
+#si se introducen mal el numero de arguemntos                                                                   
+if [ $argumentos -ne 2 ];then
+    help
+fi
+
+#asignamos arguemntos                                                                                           
+dirSSH=$1
+file=$2
+encontrado=0
+
+#buscamos la direccion remota                                                                                   
+while IFS=: read nombre direccion passwd #se deja el passwd, si en un futuro                                    
+                                         #se emplea expect                                                      
+do
+
+    if [ $dirSSH = $nombre ];then  #esto es para debug                                                          
+        echo "Se trata de: $nombre"
+        echo "su direccion es: $direccion"
+        echo "con el passwd: $passwd"
+        encontrado=1
+        break
+    fi
+done < ~/Documents/Pau/ProgsBash/maquinaRemota #aqui poner ruta del documento donde                             
+                                      #se guardan las direcciones de las Maquinas Remotas    
+
+if [ $encontrado -eq 0 ];then
+    echo "La maquina no se encuentra en la lista o esta mal escrito"
+    help
+fi
+
+#subimos el archivo mediante el comando scp                                                                     
+#local --> remoto                                                                                               
+scp $file $direccion:
+
+#vendria expect, para automatizarlo                                                                             
+
+
+#fin                                                                                                            
+exit 0
